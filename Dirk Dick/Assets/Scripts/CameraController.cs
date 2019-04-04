b268f47216f229ab2cb61f423851419adf2cc2b2
Kristoffer <Stoffer26@hotmail.com>
2019-04-04 09:38:59 +0200

    still peter

---
+++ Dirk Dick/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    public bool bounds = false;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+
+        if (bounds == true)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+    }
+}
