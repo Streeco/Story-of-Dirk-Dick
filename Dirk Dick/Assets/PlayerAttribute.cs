35441660c3ab1ae4c30ac6c6cdf42071940a04eb
Peter Kryger Kristensen <glpekri8@student.eadania.dk>
2019-04-04 12:06:41 +0200

    Scene oprettet som ny under "Peter"

---
+++ Dirk Dick/Assets/PlayerAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public class PlayerAttribute : MonoBehaviour
+{
+    DateTime now = DateTime.Now;
+    string format = "dd MM yyyy  hh:mm";
+    string loadDate;
+    string saveDate;
+    
+    Vector3 playerPos;
+    public static int Health = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       DontDestroyOnLoad(gameObject);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
